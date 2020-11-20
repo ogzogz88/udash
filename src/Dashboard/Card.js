7b647bf5e30345e2f52e83948505efa2090ce87e
Oguzhan Alagoz <ogzogz88@gmail.com>
2020-11-20 23:58:15 +0300

    Add Cards with styled-components

---
+++ src/Dashboard/Card.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+import { medGrey } from './GlobalStyle'
+
+const Card = styled.div`
+margin-top:20px;
+padding: 20px;
+background-color: white;
+box-shadow:0px 3px 5px ${medGrey};
+box-sizing:border-box;
+${({ height }) => height && `height:${height}px;`}
+@media (max-width:1200px){
+    padding:10px;
+}
+`
+
+export default Card
