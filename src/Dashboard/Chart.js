7f1dd63d4c7cca59f7ff423d2fb06fc0950aa6a5
Oguzhan Alagoz <ogzogz88@gmail.com>
2020-11-22 16:58:11 +0300

    Add Chart DarkTheme

--- src/Dashboard/Chart.js
+++ src/Dashboard/Chart.js
@@ -1,100 +1,134 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Card from './Card'
 import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
 import { chartData } from './Data'
+import { ThemeContext } from './Dashboard'
+import { darkerGrey, darkGrey, lightGrey } from './GlobalStyle'
 
 
-const options =
-{
-    chart: {
-        type: 'column',
-        height: '360px',
-        style: {
-            fontFamily: `'Blinker', 'sans-serif'`,
-        },
-        events: {
-            load: function () {
+function getOptions(dark) {
+    return {
+        chart: {
+            type: 'column',
+            height: '360px',
+            backgroundColor: dark ? darkerGrey : 'white',
 
-                this.update({
-                    legend: {
-                        labelFormatter: function () {
-                            var seriesData = this.data,
-                                pointSum = 0;
-                            seriesData.forEach(function (point) {
-                                pointSum += point.y;
-                            })
-                            return this.name + '<br />Total: ' + pointSum;
-                        }
-                    },
-                })
-            }
-        }
-    },
-    title: {
-        text: 'Revenue by Product'
-    },
-    xAxis: {
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-    },
-    yAxis: {
-        min: 0,
-        title: {
-            text: 'Thousands of Dollars'
-        },
-        stackLabels: {
-            enabled: true,
             style: {
-                fontWeight: 'bold',
-                color: ( // theme
-                    Highcharts.defaultOptions.title.style &&
-                    Highcharts.defaultOptions.title.style.color
-                ) || 'gray'
-            }
-        }
-    },
-    legend: {
-        align: 'center',
-        x: 0,
-        verticalAlign: 'bottom',
-        y: 0,
-        floating: false,
-        backgroundColor:
-            Highcharts.defaultOptions.legend.backgroundColor || 'white',
-        borderColor: '#CCC',
-        borderWidth: 1,
-        shadow: false
-    },
-    tooltip: {
-        headerFormat: '<b>{point.x}</b><br/>',
-        pointFormat: '{series.name}: {point.y}, ({point.percentage:.0f}%)<br/>',
-        shared: true
-    },
-    plotOptions: {
-        column: {
-            stacking: 'normal',
-            dataLabels: {
-                enabled: false
+                fontFamily: `'Blinker', 'sans-serif'`,
+
+            },
+            events: {
+                load: function () {
+
+                    this.update({
+                        legend: {
+                            labelFormatter: function () {
+                                var seriesData = this.data,
+                                    pointSum = 0;
+                                seriesData.forEach(function (point) {
+                                    pointSum += point.y;
+                                })
+                                return this.name + '<br />Total: ' + pointSum;
+                            }
+                        },
+                    })
+                },
+
+            },
+
+        },
+        title: {
+            text: 'Revenue by Product',
+            style: {
+                color: dark ? lightGrey : 'black'
+            },
+        },
+        xAxis: {
+            labels: {
+                style: {
+                    color: dark ? lightGrey : 'black'
+                }
+            },
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Thousands of Dollars',
+                style: {
+                    color: dark ? lightGrey : 'black'
+                }
+            },
+            labels: {
+                style: {
+                    color: dark ? lightGrey : 'black'
+                }
+            },
+            gridLineColor: dark ? 'grey' : lightGrey,
+            stackLabels: {
+                enabled: true,
+                style: {
+                    fontWeight: 'bold',
+                    color: ( // theme
+                        Highcharts.defaultOptions.title.style &&
+                        Highcharts.defaultOptions.title.style.color
+                    ) || 'gray'
+                }
             }
         },
-        series: {
-            borderWidth: 0
-        }
-    },
-    series: chartData
+        legend: {
+            align: 'center',
+            x: 0,
+            verticalAlign: 'bottom',
+            y: 0,
+            floating: false,
+            itemStyle: {
+                color: dark ? lightGrey : 'black',
+            },
+            backgroundColor:
+                dark ? darkGrey : 'white',
+            //Highcharts.defaultOptions.legend.backgroundColor || 'white',
+            borderColor: '#CCC',
+            borderWidth: 1,
+            shadow: false
+        },
+        tooltip: {
+            headerFormat: '<b>{point.x}</b><br/>',
+            pointFormat: '{series.name}: {point.y}, ({point.percentage:.0f}%)<br/>',
+            shared: true,
+            style: {
+                color: dark ? lightGrey : 'black',
+
+            },
+            backgroundColor: dark ? darkGrey : 'white'
+        },
+        plotOptions: {
+            column: {
+                stacking: 'normal',
+                dataLabels: {
+                    enabled: false
+                }
+            },
+            series: {
+                borderWidth: 0
+            }
+        },
+        series: chartData
+    }
 }
 
 
 
 function Chart() {
+    const [theme, setTheme] = useContext(ThemeContext);
+    const dark = theme === 'dark';
     return (
-        <Card>
-            <div>
-                <HighchartsReact
-                    highcharts={Highcharts}
-                    options={options}
-                />
-            </div>
+        <Card height={400} dark={dark}>
+            <HighchartsReact
+                highcharts={Highcharts}
+                options={getOptions(dark)}
+            />
         </Card>
     );
 }
--- src/Dashboard/GlobalStyle.js
+++ src/Dashboard/GlobalStyle.js
@@ -1,10 +1,11 @@
 import styled, { createGlobalStyle } from "styled-components"
 
-export const black = '#252020'
+export const black = '#252020';
 export const darkGrey = "#515263";
 export const medGrey = "#dad9d9";
 export const lightGrey = "#f7f7f7";
-export const brandColor = '#5C60C1'
+export const brandColor = '#5C60C1';
+export const darkerGrey = '#353131';
 
 const GlobalStyle = createGlobalStyle`
 body{
